bd0c578ce83402da7f9788720ee01a0c3c81a42c
Jiada Ye <jiaday@uci.edu>
2019-10-23 16:24:18 -0700

    lesson 1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+//create a simple react element
+// const createApp = (props) => {
+//     return (<div>
+//         {/*if you want to insert js code to jsx,you should add {}*/}
+//         <h1>Welcome {props.title}!!!</h1>
+//         <p>React is A JavaScript library for building user interfaces</p>
+//         <p>great {props.title}</p>
+//     </div>);
+// }
+// const app = createApp({
+//     title:'React 16.8'
+// });
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//the 1st way to create a component: use arrow function. the name starts with capital
+const App = (props) => {
+    return (<div>
+        {/*if you want to insert js code to jsx,you should add {}*/}
+        <h1 title={props.title}>Welcome {props.title}!!!</h1>
+        <p>React is A JavaScript library for building user interfaces</p>
+        <p>great {props.title}</p>
+    </div>);
+};
+
+ReactDOM.render(
+    <App title='1901'/>,
+    document.querySelector('#root')
+);
