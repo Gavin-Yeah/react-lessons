2ca712637e4beed963c533a43626e2652c7c5fbd
Jiada Ye <jiaday@uci.edu>
2019-10-24 00:20:08 -0700

    lesson 3, use class to create react component

--- src/index.js
+++ src/index.js
@@ -1,36 +1,94 @@
 import React,{Component} from 'react'
 import {render} from 'react-dom'
 
+
+
 //2nd way to define a component: use class, inherit React.Component
+// class App extends Component{
+//     render() {
+//         //console.log(this.props.title);
+//         return(
+//             <div className="app" id='appRoot'>
+//                <h1 className="title">JSC theory</h1>
+//                 <p>class component extends React.Component</p>
+//             </div>
+//
+//         )
+//             ;
+//     };
+//
+// }
+
+
+//react original component creation
 class App extends Component{
     render() {
         //console.log(this.props.title);
         return(
-            <div>
-                <h1>class component</h1>
-                <p>{this.props.title}</p>
-            </div>
+            //React.createElement is a function to create an element, having infinite parameters, but the first two must be :
+            // tag name and attribute object
+            //the rest are the children elements
+           React.createElement(
+               'div',
+               {
+                   className:'app',
+                   id:'root'
+               },
+               React.createElement(
+                   'h1',
+                   {
+                       className:'title'
+                   },
+                   'jsx theory'
+               ),
+               React.createElement(
+                   'p',
+                   null,
+                   'class component extends React.Component'
+               )
+           )
 
         )
             ;
     };
 
 }
-//the theory of class component
-// const app = new App({
-//     title:"class component extends React.Component"
-// }).render();
 
+
+
+
+//js original component creation
+// const appDom =
+// {
+//     tag:'div',
+//         attrs:{
+//         className:'app',
+//             id:'appRoot'
+//
+// },
+//     children:[
+//         {
+//         tag:'h1',
+//     attrs:{
+//     className:'title',
+//
+// },
+//     children:[
+//         'JSX theory'
+//     ]},{
+//             tag:'p',
+//             attrs: null,
+//             children:[
+//                 'class component extends React.Component'
+//             ]
+//         },
+//
+//     ]
+// };
 
 //render is provided by ReactDOM, just used once
 render(
-    <App title={"class component extends React.Component"} x="abc"/>,
+    <App />,
     document.querySelector('#root')
 );
 
-//before react 16, use this way to create
-// React.createClass({
-//     render(){
-//         return <h1>xxx</h1>
-//     }
-// })
