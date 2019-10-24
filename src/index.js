68fcbc33d348aa7ed101e0898f5f2e42fd410be0
Jiada Ye <jiaday@uci.edu>
2019-10-24 00:57:58 -0700

    lesson 4 style insert

--- src/index.js
+++ src/index.js
@@ -1,90 +1,32 @@
 import React,{Component} from 'react'
 import {render} from 'react-dom'
+import classNames from 'classnames'
+import styled from "styled-components"
 
-
-
-//2nd way to define a component: use class, inherit React.Component
-// class App extends Component{
-//     render() {
-//         //console.log(this.props.title);
-//         return(
-//             <div className="app" id='appRoot'>
-//                <h1 className="title">JSC theory</h1>
-//                 <p>class component extends React.Component</p>
-//             </div>
-//
-//         )
-//             ;
-//     };
-//
-// }
-
-
-//react original component creation
-class App extends Component{
+import './index.css'
+console.log(styled);
+const Title = styled.h1`
+color:#d00
+`;
+class App extends Component {
     render() {
-        //console.log(this.props.title);
+        const style = {color:'#F00'};
         return(
-            //React.createElement is a function to create an element, having infinite parameters, but the first two must be :
-            // tag name and attribute object
-            //the rest are the children elements
-           React.createElement(
-               'div',
-               {
-                   className:'app',
-                   id:'root'
-               },
-               React.createElement(
-                   'h1',
-                   {
-                       className:'title'
-                   },
-                   'jsx theory'
-               ),
-               React.createElement(
-                   'p',
-                   null,
-                   'class component extends React.Component'
-               )
-           )
-
+            <div>
+                <h1 style={style}>the style of the element</h1>
+                <Title> styled</Title>
+                <ol>
+                    <li>use inline style </li>
+                    <li className='has-text-red'> use class but in react class is coded as className</li>
+                    <li className={classNames('a',{'b':true, 'c':false})}>add classNames dynamically using classNames </li>
+                    <li>classNames only have a and b</li>
+                </ol>
+            </div>
         )
-            ;
-    };
-
+    }
 }
 
 
-
-
-//js original component creation
-// const appDom =
-// {
-//     tag:'div',
-//         attrs:{
-//         className:'app',
-//             id:'appRoot'
-//
-// },
-//     children:[
-//         {
-//         tag:'h1',
-//     attrs:{
-//     className:'title',
-//
-// },
-//     children:[
-//         'JSX theory'
-//     ]},{
-//             tag:'p',
-//             attrs: null,
-//             children:[
-//                 'class component extends React.Component'
-//             ]
-//         },
-//
-//     ]
-// };
 
 //render is provided by ReactDOM, just used once
 render(
---
+++ src/note.md
@@ -0,0 +1,7 @@
+##  Classnames
+- npm install classnames --save
+-  import classNames from 'classnames' 
+-   classNames('a',{'b':true, 'c':false}
+
+##  styled-components
+- npm install styled-components -s
