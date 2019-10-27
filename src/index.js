e6cb31a9ea2214ddefdfe4cf2b9c6a7e3c52a7a5
Jiada Ye <jiaday@uci.edu>
2019-10-26 19:49:50 -0700

    context

--- src/index.js
+++ src/index.js
@@ -1,27 +1,21 @@
-//need to import useState useEffect
-import React, {useState, useEffect} from 'react'
+
+import React,{Component} from 'react'
 import {render} from 'react-dom'
 
-//react hooks makes function have own state
-const Counter = ()=>{
-    //useState is a function, parameter is the initial state. this function returns an array [state,setState]
-    const [count,setCount] = useState(0);
-    //useEffect is a callback function. when update ot load, it will be called, like the combination of componentMount and componentUpdate
-    useEffect(()=>{
-        console.log('update')
-        document.title = `当前的数量为${count}`;
-    })
-    return(
-        <div>
-            <p>当前的数量为{count}</p>
+class Counter extends
 
-            <button onClick={()=>{setCount(count-1)}}>-</button>
 
-            <span>{count}</span>
 
-            <button onClick={()=>{setCount(count+1)}}>+</button>
-        </div>
-    )
+class App extends Component{
+    render() {
+        return(
+            <>
+                <CountBtn type={"decrement"}/>
+                <Counter/>
+                <CountBtn type={"increment"}/>
+            </>
+        )
+    }
 }
 
 render(
